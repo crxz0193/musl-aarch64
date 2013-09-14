ff61f24f8111435e0e97885c5bf47e6fc2ab969e
Kevin Bortis <kevin@bortis.ch>
2013-09-14 14:46:30 +0200

    Added bits/float.h

---
+++ arch/aarch64/bits/float.h
@@ -0,0 +1,20 @@
+#define FLT_ROUNDS 1
+#define FLT_EVAL_METHOD 0
+
+#define LDBL_TRUE_MIN 4.9406564584124654e-324
+#define LDBL_MIN 2.2250738585072014e-308
+#define LDBL_MAX 1.7976931348623157e+308
+#define LDBL_EPSILON 2.2204460492503131e-16L
+
+#define LDBL_MANT_DIG 53
+#define LDBL_MIN_EXP (-1021)
+#define LDBL_MAX_EXP 1024
+
+#define LDBL_DIG 15
+#define LDBL_MIN_10_EXP (-307)
+#define LDBL_MAX_10_EXP 308
+
+#define DECIMAL_DIG 17
+
+/* FIXME: Only reference found is arm compiler reference manual, wich does  not mention aarch64
+ * http://infocenter.arm.com/help/topic/com.arm.doc.dui0491i/DUI0491I_arm_compiler_reference.pdf */
