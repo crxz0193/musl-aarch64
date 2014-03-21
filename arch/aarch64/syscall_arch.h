c176214bd5b44d5d45aced9c5fcac28f9fb89ac3
Sireesh Tripurari <sireesh.tripurari@linaro.org>
2014-03-21 17:56:16 +0530

    enable generic syscall

--- arch/aarch64/syscall_arch.h
+++ arch/aarch64/syscall_arch.h
@@ -3,54 +3,53 @@
 ((union { long long ll; long l[2]; }){ .ll = x }).l[1]
 #define __SYSCALL_LL_O(x) 0, __SYSCALL_LL_E((x))
 
-#if 0
-//#ifndef __clang__
+#ifndef __clang__
 
 #define __asm_syscall(...) do { \
 	__asm__ __volatile__ ( "svc 0" \
-	: "=r"(r0) : __VA_ARGS__ : "memory"); \
-	return r0; \
+	: "=r"(x0) : __VA_ARGS__ : "memory"); \
+	return x0; \
 	} while (0)
 
 static inline long __syscall0(long n)
 {
-	register long r7 __asm__("r7") = n;
-	register long r0 __asm__("r0");
-	__asm_syscall("r"(r7));
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0");
+	__asm_syscall("r"(x8));
 }
 
 static inline long __syscall1(long n, long a)
 {
-	register long r7 __asm__("r7") = n;
-	register long r0 __asm__("r0") = a;
-	__asm_syscall("r"(r7), "0"(r0));
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = a;
+	__asm_syscall("r"(x8), "0"(x0));
 }
 
 static inline long __syscall2(long n, long a, long b)
 {
-	register long r7 __asm__("r7") = n;
-	register long r0 __asm__("r0") = a;
-	register long r1 __asm__("r1") = b;
-	__asm_syscall("r"(r7), "0"(r0), "r"(r1));
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = a;
+	register long x1 __asm__("x1") = b;
+	__asm_syscall("r"(x8), "0"(x0), "r"(x1));
 }
 
 static inline long __syscall3(long n, long a, long b, long c)
 {
-	register long r7 __asm__("r7") = n;
-	register long r0 __asm__("r0") = a;
-	register long r1 __asm__("r1") = b;
-	register long r2 __asm__("r2") = c;
-	__asm_syscall("r"(r7), "0"(r0), "r"(r1), "r"(r2));
+	register long x8 __asm__("r8") = n;
+	register long x0 __asm__("r0") = a;
+	register long x1 __asm__("r1") = b;
+	register long x2 __asm__("r2") = c;
+	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2));
 }
 
 static inline long __syscall4(long n, long a, long b, long c, long d)
 {
-	register long r7 __asm__("r7") = n;
-	register long r0 __asm__("r0") = a;
-	register long r1 __asm__("r1") = b;
-	register long r2 __asm__("r2") = c;
-	register long r3 __asm__("r3") = d;
-	__asm_syscall("r"(r7), "0"(r0), "r"(r1), "r"(r2), "r"(r3));
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = a;
+	register long x1 __asm__("x1") = b;
+	register long x2 __asm__("x2") = c;
+	register long x3 __asm__("x3") = d;
+	__asm_syscall("r"(x8), "0"(x0), "r"(x1), "r"(x2), "r"(x3));
 }
 
 #else
