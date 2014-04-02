a3b120a7cce63db547ba9286a5f9876a7d4562da
Sireesh Tripurari <sireesh.tripurari@linaro.org>
2014-04-02 11:50:04 +0530

    support for unlinkat call

--- src/unistd/unlink.c
+++ src/unistd/unlink.c
@@ -1,7 +1,12 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include "syscall.h"
 
 int unlink(const char *path)
 {
+#ifdef SYS_unlinkat
+	return syscall(SYS_unlinkat, AT_FDCWD, path,0);/*flags specifed as 0*/
+#else
 	return syscall(SYS_unlink, path);
+#endif
 }
